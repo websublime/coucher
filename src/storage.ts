32f599a05dbd409b46aaa7443571975084f174e9
Miguel Ramos <miguel.marques.ramos@gmail.com>
2019-10-11 08:03:55 +0100

    Storage methods implementation

--- src/storage.ts
+++ src/storage.ts
@@ -1,9 +1,24 @@
-import { Bucket } from "couchbase";
-import { useBucketStorageError } from "./utils";
-import { CouchGetResponse } from "./types/coucher";
+import {
+  Bucket,
+  GetAndLockOptions,
+  InsertOptions,
+  AppendOptions,
+  PrependOptions,
+  CounterOptions,
+  N1qlQuery,
+  ViewQuery,
+  SpatialQuery,
+  RemoveOptions,
+  ReplaceOptions,
+  UpsertOptions,
+  TouchOptions,
+  CreateIndexOptions,
+  DropIndexOptions,
+} from 'couchbase';
+import { useBucketStorageError } from './utils';
+import { CouchGetResponse } from './types/coucher';
 
 export class Storage {
-
   protected bucket: Bucket;
 
   private bucketName: string;
@@ -29,7 +44,7 @@ export class Storage {
     this.bucket.disconnect();
   }
 
-  public async get<T>(key: string, options = {}) {
+  public async get<T = any>(key: string, options = {}) {
     return new Promise<CouchGetResponse<T>>((resolve, reject) => {
       this.bucket.get(key, options, (err, result) => {
         return err ? reject(useBucketStorageError(err.message)) : resolve(result);
@@ -41,15 +56,216 @@ export class Storage {
     return new Promise((resolve, reject) => {
       this.bucket.getMulti(keys, (err, result) => {
         return err ? reject(useBucketStorageError(err.toString())) : resolve(result);
-      })
+      });
     });
   }
 
-  public async getAndTouch(key: string, expiry: number, options = {}) {
-    return new Promise((resolve, reject) => {
+  public async getAndTouch<T = any>(key: string, expiry: number, options = {}) {
+    return new Promise<CouchGetResponse<T>>((resolve, reject) => {
       this.bucket.getAndTouch(key, expiry, options, (err, result) => {
         return err ? reject(useBucketStorageError(err.message)) : resolve(result);
       });
+    });
+  }
+
+  public async getAndLock<T = any>(key: string, options: GetAndLockOptions = {}) {
+    return new Promise<CouchGetResponse<T>>((resolve, reject) => {
+      this.bucket.getAndLock(key, options, (err, result) => {
+        return err ? reject(useBucketStorageError(err.message)) : resolve(result);
+      });
+    });
+  }
+
+  public async insert<T = any>(key: string, data: {}, options: InsertOptions = {}) {
+    return new Promise<CouchGetResponse<T>>((resolve, reject) => {
+      this.bucket.insert(key, data, options, (err, result) => {
+        return err ? reject(useBucketStorageError(err.message)) : resolve(result);
+      });
+    });
+  }
+
+  public async append<T = any>(key: string, value: string, options: AppendOptions = {}) {
+    return new Promise<CouchGetResponse<T>>((resolve, reject) => {
+      this.bucket.append(key, value, options, (err, result) => {
+        return err ? reject(useBucketStorageError(err.message)) : resolve(result);
+      });
+    });
+  }
+
+  public async prepend<T = any>(key: string, value: string, options: PrependOptions = {}) {
+    return new Promise<CouchGetResponse<T>>((resolve, reject) => {
+      this.bucket.prepend(key, value, options, (err, result) => {
+        return err ? reject(useBucketStorageError(err.message)) : resolve(result);
+      });
+    });
+  }
+
+  public async counter<T = any>(key: string, delta: number = 1, options: CounterOptions = {}) {
+    return new Promise<CouchGetResponse<T>>((resolve, reject) => {
+      this.bucket.counter(key, delta, options, (err, result) => {
+        return err ? reject(useBucketStorageError(err.message)) : resolve(result);
+      });
+    });
+  }
+
+  public async query(query: N1qlQuery, params = {}) {
+    return new Promise<any[]>((resolve, reject) => {
+      this.bucket.query(query, params, (err, result) => {
+        return err ? reject(useBucketStorageError(err.message)) : resolve(result);
+      });
+    });
+  }
+
+  public async view(query: SpatialQuery | ViewQuery) {
+    return new Promise((resolve, reject) => {
+      this.bucket.query(query, (err, result) => {
+        return err ? reject(useBucketStorageError(err.message)) : resolve(result);
+      });
+    });
+  }
+
+  public async executeQuery(
+    query: string,
+    params: {},
+    options: { consistency?: N1qlQuery.Consistency } = {},
+  ): Promise<any[]> {
+    return new Promise<any[]>((resolve, reject) => {
+      let n1ql = N1qlQuery.fromString(query);
+
+      if (options.consistency) {
+        n1ql.consistency(options.consistency);
+      }
+
+      this.query(n1ql, params)
+        .then((results: any[]) => {
+          resolve(
+            results.map(result => {
+              return result[this.getBucketName()] || result;
+            }),
+          );
+        })
+        .catch(err => reject(useBucketStorageError(err.message)));
+    });
+  }
+
+  public async remove(key: string, options: RemoveOptions = {}) {
+    return new Promise<{ cas: Bucket.CAS; token?: any }>((resolve, reject) => {
+      this.bucket.remove(key, options, (err, result) => {
+        return err ? reject(useBucketStorageError(err.message)) : resolve(result);
+      });
+    });
+  }
+
+  public async replace<T = any>(key: string, value: any, options: ReplaceOptions = {}) {
+    return new Promise<CouchGetResponse<T>>((resolve, reject) => {
+      this.bucket.replace(key, value, options, (err, result) => {
+        return err ? reject(useBucketStorageError(err.message)) : resolve(result);
+      });
+    });
+  }
+
+  public async upsert<T = any>(key: string, value: any, options: UpsertOptions = {}) {
+    return new Promise<CouchGetResponse<T>>((resolve, reject) => {
+      this.bucket.upsert(key, value, options, (err, result) => {
+        return err ? reject(useBucketStorageError(err.message)) : resolve(result);
+      });
+    });
+  }
+
+  public async touch<T = any>(key: string, expiry: number, options: TouchOptions = {}) {
+    return new Promise<CouchGetResponse<T>>((resolve, reject) => {
+      this.bucket.touch(key, expiry, options, (err, result) => {
+        return err ? reject(useBucketStorageError(err.message)) : resolve(result);
+      });
+    });
+  }
+
+  public async unlock(key: string, cas: Bucket.CAS, options: any = {}): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+      this.bucket.unlock(key, cas, options, (err, result) => {
+        return err ? reject(useBucketStorageError(err.message)) : resolve(result);
+      });
+    });
+  }
+
+  public async exists(key: string): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+      this.insert(key, true, { expiry: 1 })
+        .then(() => resolve(false))
+        .catch((err: Error) => {
+          return err ? resolve(true) : reject(err);
+        });
+    });
+  }
+
+  public async createIndex(
+    name: string,
+    indexFields: string[],
+    options: CreateIndexOptions = {},
+  ): Promise<boolean> {
+    return new Promise<boolean>((resolve, _reject) => {
+      this.bucket.manager().createIndex(name, indexFields, options, err => {
+        return err ? resolve(false) : resolve(true);
+      });
+    });
+  }
+
+  public async dropIndex(name: string, options: DropIndexOptions = {}): Promise<boolean> {
+    return new Promise<boolean>((resolve, _reject) => {
+      this.bucket.manager().dropIndex(name, options, err => {
+        return err ? resolve(false) : resolve(true);
+      });
+    });
+  }
+
+  public async getIndexes(
+    options: { consistency?: N1qlQuery.Consistency } = {},
+  ): Promise<Array<{}>> {
+    return new Promise<Array<{}>>((resolve, reject) => {
+      const query = 'SELECT `indexes`.* FROM system:indexes';
+
+      const n1ql = N1qlQuery.fromString(query);
+
+      if (options.consistency) {
+        n1ql.consistency(options.consistency);
+      }
+
+      this.query(n1ql, {})
+        .then(results => resolve(results))
+        .catch(err => reject(err));
+    });
+  }
+
+  public async buildDeferredIndexes(): Promise<boolean> {
+    return new Promise<boolean>((resolve, reject) => {
+      this.getIndexes()
+        .then(async (indexes: any) => {
+          let deferredList = indexes.reduce((carry: any, index: any) => {
+            if (['deferred', 'pending'].includes(index.state)) {
+              carry.push('`' + index.name + '`');
+            }
+
+            return carry;
+          }, []);
+
+          if (deferredList.length === 0) {
+            resolve(true);
+          }
+
+          const query =
+            'BUILD INDEX ON ' +
+            '`' +
+            this.getBucketName() +
+            '` ' +
+            '(' +
+            deferredList.join(', ') +
+            ')';
+
+          await this.executeQuery(query, {});
+
+          resolve(true);
+        })
+        .catch(_err => resolve(false));
     });
   }
 }
--- test/storage.test.ts
+++ test/storage.test.ts
@@ -2,8 +2,11 @@ import { Cluster, Bucket, N1qlQuery } from 'couchbase';
 import { useBucketConnectionError } from '../src/utils';
 import { Storage } from '../src/storage';
 import { config } from 'dotenv';
+import { v4 } from 'uuid';
 
 config();
+
+jest.setTimeout(30000);
 
 const connect = async (): Promise<Bucket> => {
   return new Promise((resolve, reject) => {
@@ -27,9 +30,13 @@ const connect = async (): Promise<Bucket> => {
 describe('> Storage', () => {
   let storage: Storage;
 
-  beforeAll(async () => connect().then(bucket => {
-    return storage = new Storage(bucket, 'travel-sample');
-  }), 10000);
+  beforeAll(
+    async () =>
+      connect().then(bucket => {
+        return (storage = new Storage(bucket, 'travel-sample'));
+      }),
+    10000,
+  );
 
   afterAll(() => storage.disconnect());
 
@@ -40,20 +47,139 @@ describe('> Storage', () => {
   it('# Should get document from template', async () => {
     expect.assertions(1);
 
-    const result = await storage.get('airline_10', {
-      consistency: N1qlQuery.Consistency.REQUEST_PLUS
+    const result = await storage.get('airline_10123', {
+      consistency: N1qlQuery.Consistency.REQUEST_PLUS,
     });
 
     expect(Object.keys(result).includes('cas')).toBeTruthy();
   });
 
   it('# Should get multi documents from template', async () => {
-    expect.assertions(3);
+    expect.assertions(2);
 
-    const results = await storage.getMulti(['airline_10', 'airline_10123', 'airline_109']);
+    const results = await storage.getMulti(['airline_10123', 'airline_109']);
 
-    expect(Object.keys(results).includes('airline_10')).toBeTruthy();
     expect(Object.keys(results).includes('airline_10123')).toBeTruthy();
     expect(Object.keys(results).includes('airline_109')).toBeTruthy();
+  });
+
+  it('# Should get touch document', async () => {
+    const result = await storage.getAndTouch('airline_112', 0);
+
+    expect(Object.keys(result).includes('cas')).toBeTruthy();
+  });
+
+  it('# Should get and lock document', async () => {
+    const result = await storage.getAndLock('airline_1191', { lockTime: 5 });
+
+    expect(Object.keys(result).includes('cas')).toBeTruthy();
+  });
+
+  xit('# Should insert a new document', async () => {
+    const result = await storage.insert(
+      'airline_116',
+      {
+        callsign: 'ACE AIR',
+        country: 'United States',
+        iata: 'KA',
+        icao: 'AKE',
+        id: 117,
+        name: 'Kansas Express',
+        type: 'airline',
+      },
+      { expiry: 0 },
+    );
+
+    expect(Object.keys(result).includes('cas')).toBeTruthy();
+  });
+
+  xit('# Should append to an exist document', async () => {
+    const _ = await storage.insert('airline_118', 'string');
+    const result = await storage.append('airline_118', 'one_new_value');
+
+    expect(Object.keys(result).includes('cas')).toBeTruthy();
+  });
+
+  xit('# Should prepend to an exist document', async () => {
+    const _ = await storage.insert('airline_117', 'string');
+    const result = await storage.prepend('airline_117', 'one_new_value');
+
+    expect(Object.keys(result).includes('cas')).toBeTruthy();
+  });
+
+  it('# Should create a counter', async () => {
+    const _ = await storage.insert('counter', 100);
+    const result = await storage.counter('counter', 1, { initial: 100 });
+
+    expect(Object.keys(result).includes('cas')).toBeTruthy();
+  });
+
+  it('# Should execute query', async () => {
+    const query = N1qlQuery.fromString('SELECT type FROM `travel-sample` LIMIT 4');
+    const queryPar = N1qlQuery.fromString('SELECT name FROM `travel-sample` WHERE name=$1');
+
+    const result: { type: string }[] = await storage.query(query);
+    const resultQuery: { name: string }[] = await storage.query(queryPar, ['Texas Wings']);
+
+    expect(result.length > 0).toBeTruthy();
+    expect(resultQuery.length > 0).toBeTruthy();
+  });
+
+  it('# Should execute raw query', async () => {
+    const result: { type: string }[] = await storage.executeQuery(
+      'SELECT name FROM `travel-sample` WHERE name=$1',
+      ['Texas Wings'],
+      { consistency: N1qlQuery.Consistency.REQUEST_PLUS },
+    );
+
+    expect(result.length > 0).toBeTruthy();
+  });
+
+  it('# Should remove a document', async () => {
+    const result = await storage.remove('counter');
+
+    expect(Object.keys(result).includes('cas')).toBeTruthy();
+  });
+
+  it('# Should replace a document', async () => {
+    const result = await storage.replace(
+      'airline_116',
+      {
+        callsign: 'ACE AIR',
+        country: 'United States',
+        iata: 'KA',
+        icao: 'AKE',
+        id: 117,
+        name: 'Texas Express',
+        type: 'airline',
+      },
+      { expiry: 0 },
+    );
+
+    expect(Object.keys(result).includes('cas')).toBeTruthy();
+  });
+
+  it('# Should update or insert a document', async () => {
+    const result = await storage.upsert(
+      'airline_116',
+      {
+        callsign: 'ACE AIR',
+        country: 'United States',
+        iata: 'KA',
+        icao: 'AKE',
+        id: 117,
+        name: 'California Express',
+        type: 'airline',
+      },
+      { expiry: 0 },
+    );
+
+    expect(Object.keys(result).includes('cas')).toBeTruthy();
+  });
+
+  it('# Should check document exist', async () => {
+    const result = await storage.exists('airline_116');
+
+    expect(result).toBeTruthy();
   });
 });
